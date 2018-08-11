e2f94549e942d45030aa7be099aab585ab3f34ad
Nikndj <nicola.delgiudice@studenti.unicam.it>
2018-08-11 10:26:53 +0200

    Login #1

--- server/app/controllers/users.js
+++ server/app/controllers/users.js
@@ -2,6 +2,27 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 exports.create = (req, res) => {
+    var password = req.body.password;
+    var passwordConfirm = req.body.passwordConfirm;
+
+    if (password===passwordConfirm) {
+        var user= new User();
+        user.name = req.body.name; 
+        user.surname = req.body.surname;
+        user.email = req.body.email;
+        user.hashed_password = CryptoJS.MD5("password");
+        user.save();
+        if (user.save()) {
+            return res.redirect ("/");
+        }
+    }
     
 };
 
+exports.delete=(req,res) =>{
+
+};
+
+exports.find= (req,res) =>{
+
+};
--- server/config/routes.js
+++ server/config/routes.js
@@ -6,4 +6,30 @@ module.exports = function(app) {
         console.log('homepage');
         res.render('homepage.ejs');
     });
+
+    app.get('/login', (req, res) => {
+        console.log ('login');
+        res.render('login.ejs');
+    });
+
+    app.get('/register', (req, res) => {
+        console.log ('register');
+        res.render('register.ejs');
+    });
+    
+    app.get('/profile', (req, res) => {
+        console.log ('profile');
+        res.render(/*nome pagina profilo*/);
+    });
+
+    app.post('/registraUtente', (req,res) => {
+        console.log ('registra utente');
+        users.create;
+    });
+
+    app.post('/loginUtente', (req,res)=>{
+        console.log('login utente');
+        users.find;
+    });
+    
 }
