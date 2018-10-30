5bfbbf68330c429211c1278010123cbd0e04b411
PORTATILEMARCO\Marco03 <marco0395@gmail.com>
2018-10-30 12:40:41 +0100

    Implementazione gestione studenti e prenotazioni da parte dell'admin

--- front/gestione-aule-studio/src/pages/admin/admin.ts
+++ front/gestione-aule-studio/src/pages/admin/admin.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ItemSliding, ModalController, AlertController, LoadingController } from 'ionic-angular';
-import { StudyRoomPage } from '../study-room/study-room';
-import { StudyRoomProvider } from '../../providers/study-room/study-room';
+import { StudentsManagementPage } from '../students-management/students-management';
+import { HomePage } from '../home/home';
 import { AuthProvider } from '../../providers/auth/auth';
-import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
-import { EditStudyRoomPage } from '../edit-study-room/edit-study-room';
+import { StudyRoomsManagementPage } from '../study-rooms-management/study-rooms-management';
 
-/**
- * Generated class for the AdminPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -19,60 +12,20 @@ import { EditStudyRoomPage } from '../edit-study-room/edit-study-room';
   templateUrl: 'admin.html',
 })
 export class AdminPage {
- items;
- studyRooms: any;
- loading: any;
 
- constructor(public navCtrl: NavController, public navParams: NavParams, public studyRoomService: StudyRoomProvider,public modalCtrl: ModalController,
-  public alertCtrl: AlertController, public authService: AuthProvider, public loadingCtrl: LoadingController) {
+ constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthProvider) {
   }
 
-   ionViewWillEnter(){
-    this.studyRoomService.getStudyRooms().then((data) => {
-      this.studyRooms = data;
-    }, (err) => {
-      let alert = this.alertCtrl.create({
-        title: 'Oooops!',
-        message: 'C\'è stato un errore, non è stato possibile caricare le informazioni delle aule',
-        buttons: ['Ok']
-      });
-      alert.present();
-    });
-  } 
-
-  addStudyRoom(){
-    this.navCtrl.push(StudyRoomPage);
+  studyRoomsManagement(){
+    this.navCtrl.push(StudyRoomsManagementPage);
   }
 
-  removeStudyRoom(studyRoom){
-    this.showLoader();
-    this.studyRoomService.deleteStudyRoom(studyRoom.name).then((result) => {
-      this.loading.dismiss();
-
-        let index = this.studyRooms.indexOf(studyRoom);
-        
-        if (index > -1) {
-          this.studyRooms.splice(index, 1);
-        }
-
-    }, (err) => {
-      this.loading.dismiss();
-        console.log("Aula non trovata");
-    });
+  studentsManagement(){
+    this.navCtrl.push(StudentsManagementPage);
   }
 
-  editStudyRoom(studyRoom){
-    console.log("ciao");
-    this.navCtrl.push(EditStudyRoomPage, {data: studyRoom});
-  }
-
-  showLoader(){
-
-    this.loading = this.loadingCtrl.create({
-      content: 'Creazione Aula...'
-    });
-
-    this.loading.present();
-
+  logout(){
+    this.authService.logout();
+    this.navCtrl.setRoot(HomePage);
   }
 }
---
+++ front/gestione-aule-studio/src/pages/study-rooms-management/study-rooms-management.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ItemSliding, ModalController, AlertController, LoadingController } from 'ionic-angular';
+import { StudyRoomPage } from '../study-room/study-room';
+import { StudyRoomProvider } from '../../providers/study-room/study-room';
+import { AuthProvider } from '../../providers/auth/auth';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
+import { EditStudyRoomPage } from '../edit-study-room/edit-study-room';
+
+
+/**
+ * Generated class for the StudyRoomsManagementPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-study-rooms-management',
+  templateUrl: 'study-rooms-management.html',
+})
+export class StudyRoomsManagementPage {
+  items;
+  studyRooms: any;
+  loading: any;
+ 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public studyRoomService: StudyRoomProvider,public modalCtrl: ModalController,
+   public alertCtrl: AlertController, public authService: AuthProvider, public loadingCtrl: LoadingController) {
+   }
+ 
+    ionViewWillEnter(){
+     this.studyRoomService.getStudyRooms().then((data) => {
+       this.studyRooms = data;
+     }, (err) => {
+       let alert = this.alertCtrl.create({
+         title: 'Oooops!',
+         message: 'C\'è stato un errore, non è stato possibile caricare le informazioni delle aule',
+         buttons: ['Ok']
+       });
+       alert.present();
+     });
+   } 
+ 
+   addStudyRoom(){
+     this.navCtrl.push(StudyRoomPage);
+   }
+ 
+   removeStudyRoom(studyRoom){
+     this.showLoader();
+     this.studyRoomService.deleteStudyRoom(studyRoom.name).then((result) => {
+       this.loading.dismiss();
+ 
+         let index = this.studyRooms.indexOf(studyRoom);
+         
+         if (index > -1) {
+           this.studyRooms.splice(index, 1);
+         }
+ 
+     }, (err) => {
+       this.loading.dismiss();
+         console.log("Aula non trovata");
+     });
+   }
+ 
+   editStudyRoom(studyRoom){
+     console.log("ciao");
+     this.navCtrl.push(EditStudyRoomPage, {data: studyRoom});
+   }
+ 
+   getStudyRooms(ev) {
+    var val = ev.target.value;
+    if (val && val.trim() != '') {
+      this.studyRooms = this.studyRooms.filter((studyRoom) => {
+        return (studyRoom.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }    
+   showLoader(){
+ 
+     this.loading = this.loadingCtrl.create({
+       content: 'Creazione Aula...'
+     });
+ 
+     this.loading.present();
+ 
+   }
+ }
